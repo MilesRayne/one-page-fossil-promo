28884f1b02e1051b0a32cbef744fd2000c9207ea
Milos Rajkovic <milosrajkovic98@yahoo.com>
2020-09-19 20:25:40 +0200

    skripta za popunjavanje product details

--- js/utils.js
+++ js/utils.js
@@ -13,7 +13,6 @@ function reduceHeaderSize() {
             link.style.padding = "5px 0px";
             link.style.width = "150px";
         }
-        console.log("radim to");
     } else {
         document.getElementById("main-logo").style.fontSize = "4rem";
         for (link of x) {
@@ -23,3 +22,70 @@ function reduceHeaderSize() {
         }
     }
 }
+
+(function fillDetails() {
+
+    let details = [{
+            label: "Sku",
+            value: "ES4519"
+        },
+        {
+            label: "Case Size",
+            value: "38MM"
+        },
+        {
+            label: "Movement",
+            value: "Quartz Multifunction"
+        },
+        {
+            label: "Platform",
+            value: "RILEY"
+        },
+        {
+            label: "Strap Material",
+            value: "Stainless Steel"
+        },
+        {
+            label: "Water Resistance",
+            value: "5 ATM"
+        },
+        {
+            label: "Case Color",
+            value: "Rose Gold"
+        },
+        {
+            label: "Dial Color",
+            value: "Rose Gold"
+        },
+        {
+            label: "Strap Fashion Color",
+            value: "Rose Gold"
+        },
+        {
+            label: "Strap Width",
+            value: "18MM"
+        },
+        {
+            label: "Closure",
+            value: "2 Pusher Foldover Clasp"
+        },
+        {
+            label: "Battery Type",
+            value: "SR621SW"
+        }
+    ];
+
+    let wrapper = document.querySelector(".detail-content");
+
+    for (element of details) {
+        wrapper.innerHTML += `<div class="attribute">
+        <span class="label">
+            ${element.label}:
+        </span>
+        <span class="value">
+            ${element.value}
+        </span>
+    </div>`
+    }
+
+})()
